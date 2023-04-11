7c032ef970c029d821b13c5e1ecee5af48acf800
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-04-11 15:12:18 +0300

    test mt19937

--- mt19937/mt19937/main.cpp
+++ mt19937/mt19937/main.cpp
@@ -1,10 +1,19 @@
+// C++ program for demonstrating 
+// similaritites
+#include <ctime>
 #include <iostream>
 #include <random>
+using namespace std;
 
 int main()
 {
-	//coment
-	std::mt19937;
+	// Initializing the sequence 
+	// with a seed value
+	// similar to srand()
+	mt19937 mt(time(nullptr));
 
+	// Printing a random number
+	// similar to rand()
+	cout << mt() << '\n';
 	return 0;
 }
