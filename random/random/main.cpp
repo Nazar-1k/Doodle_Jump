782af468ed3e631f42a2551005a1032325bdcb38
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-04-17 22:16:30 +0300

    linear_congruential_engine

--- random/random/main.cpp
+++ random/random/main.cpp
@@ -1,12 +1,19 @@
-#include <ctime>
 #include <iostream>
 #include <random>
 
-int main()
-{
-	
-	std::mt19937 mt(time(nullptr));
+int main() {
 
-	std::cout << mt.mask_bits << '\n';
+	
+#pragma region linear_congruential_engine
+
+	std::linear_congruential_engine<unsigned int, 48271, 0, 2147483647> generator;
+
+	generator.seed(std::random_device()());
+	std::uniform_int_distribution<int> distribution(0, 100);
+
+	std::cout << "linear_congruential_engine -> " << distribution(generator) << std::endl;
+	
+#pragma endregion 
+
 	return 0;
 }
