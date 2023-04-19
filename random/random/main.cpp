7a143386481a55562bb67edb0e71a34c2d94a395
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-04-19 14:58:43 +0300

    subtract_with_carry_engine

--- random/random/main.cpp
+++ random/random/main.cpp
@@ -4,28 +4,41 @@
 
 int main() {
 
-	
+std::uniform_int_distribution<int> dist(0, 99);
 #pragma region linear_congruential_engine
 
 	std::linear_congruential_engine<unsigned int, 48271, 0, 2147483647> generator;
 
 	generator.seed(std::random_device()());
-	std::uniform_int_distribution<int> distribution(0, 100);
+	
 
-	std::cout << "linear_congruential_engine -> " << distribution(generator) << std::endl;
+	std::cout << "linear_congruential_engine -> " << dist(generator) << std::endl;
 	
 #pragma endregion 
-
 
 #pragma region mersenne_twister_engine
 	
 	std::mt19937 gen(std::random_device{}());
 
-	std::uniform_int_distribution<int> dist(0, 99);
+	
 
 	std::cout << "mersenne_twister_engine -> " << dist(gen) << std::endl;
 	
 
 #pragma endregion
+
+#pragma region subtract_with_carry_engine
+
+	std::subtract_with_carry_engine<unsigned, 24, 10, 24> rng;
+
+	std::seed_seq seed{ 1, 2, 3, 4, 5 };
+	rng.seed(seed);
+
+	std::cout << "subtract_with_carry_engine -> " << dist(rng) << std::endl;
+
+
+#pragma endregion
+	
+
 	return 0;
 }
